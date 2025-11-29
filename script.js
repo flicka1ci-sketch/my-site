fdc69b0625548e88615e47c25be13e54a89c558b
flicka1ci-sketch <flicka1ci@gmail.com>
2025-11-29 17:58:29 +0200

    Add comments for clarity in script.js
    Added comments to clarify code functionality.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Поставим текущий год
   const yearSpan = document.getElementById('year');
   yearSpan.textContent = new Date().getFullYear();
 
+  // Кнопка alert
   const btn = document.getElementById('btn');
   btn.addEventListener('click', function () {
     alert("Кнопка працює!");
